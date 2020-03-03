9ff9c32a713ce42e10b3f2d066a8a88ea42d2eed
André Hashiguchi <alshashiguchi@gmail.com>
2020-03-02 23:08:17 -0300

    Utilizando Repository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,21 +3,24 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+  @Autowired
+  private TopicoRepository topicoRepository;
+
   @RequestMapping("/topicos")  
   public List<TopicoDto> lista() {
-    Topico topico = new Topico("Dúvida", "Duvida com spring", new Curso("Spring", "Programação"));
-
-    return TopicoDto.convert(Arrays.asList(topico, topico, topico));
+    List<Topico> topicos = topicoRepository.findAll();
+    return TopicoDto.convert(topicos);
   }
 
 }

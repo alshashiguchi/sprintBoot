587a3b40dcc6a0ad2f39d8ee0a9a0c1f886bba8a
André Hashiguchi <alshashiguchi@gmail.com>
2020-03-03 22:03:11 -0300

    Validações com Bean Validation

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,12 +1,23 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
+  
+  @NotNull @NotEmpty @Length(min = 5)
   private String titulo;
+
+  @NotNull @NotEmpty @Length(min = 10)
   private String mensagem;
+  
+  @NotNull @NotEmpty
   private String nomeCurso;
 
   public String getTitulo() {

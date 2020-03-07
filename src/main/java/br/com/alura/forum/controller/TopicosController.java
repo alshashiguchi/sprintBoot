d14b62386ddec51bfa0b672e5930e13150d4b0df
André Hashiguchi <alshashiguchi@gmail.com>
2020-03-07 00:39:27 -0300

    Ordenando registros na lista

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,9 +42,9 @@ public class TopicosController {
 
   // @RequestMapping(value="/topicos", method=RequestMethod.GET) 
   @GetMapping
-  public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, int pagina, int qtd) {
+  public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, int pagina, int qtd, @RequestParam String ordenacao) {
 
-    Pageable paginacao = PageRequest.of(pagina, qtd);
+    Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 
     if(nomeCurso == null) {
       Page<Topico> topicos = topicoRepository.findAll(paginacao);

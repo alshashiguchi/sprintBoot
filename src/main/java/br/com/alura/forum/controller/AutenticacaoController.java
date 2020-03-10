a0a7b3e22b2e8348248f7821e321cfcd37808fdd
André Hashiguchi <alshashiguchi@gmail.com>
2020-03-09 22:51:48 -0300

    Configurando autenticação Stateless / Grando tokens JWT

---
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,44 @@
+package br.com.alura.forum.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.form.LoginForm;
+import br.com.alura.forum.config.security.TokenService;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController{
+
+  @Autowired
+  private AuthenticationManager authManager;
+
+  @Autowired
+  private TokenService tokenService;
+
+  @PostMapping
+  public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
+    UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+    try {
+      Authentication autentication =authManager.authenticate(dadosLogin);
+    
+      String token = tokenService.gerarToken(autentication);
+
+      return ResponseEntity.ok().build();  
+    } catch (AuthenticationException e) {
+      return ResponseEntity.badRequest().build();
+    }
+    
+  }
+}

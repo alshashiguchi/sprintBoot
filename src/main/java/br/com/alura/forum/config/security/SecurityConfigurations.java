7aaa0e7d84d52e388583427ab8115e842e715d6b
André Hashiguchi <alshashiguchi@gmail.com>
2020-03-12 22:49:06 -0300

    Monitoramento com Spring Boot Actuator

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -48,6 +48,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
       .antMatchers(HttpMethod.GET, "/topicos").permitAll()
       .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
       .antMatchers(HttpMethod.POST, "/auth").permitAll()
+      .antMatchers(HttpMethod.GET, "/actuator").permitAll()
+      .antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
       .anyRequest().authenticated()
       .and().csrf().disable()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
